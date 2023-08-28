6efbf4cc7f3dfa6439fa351bf820f6c91d20ae9b
wisdomin121@naver.com <wisdomin121@naver.com>
2023-08-28 21:36:06 +0900

    feat: SignButton 생성
    - 로그인/로그아웃에 쓰일 버튼을 생성하였습니다.

---
+++ src/components/core/Button/SignButton.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const SignButtonStyle = style({
+  margin: '0 0 0 auto',
+  padding: '5px 32px',
+  border: '1px solid black',
+  borderRadius: '20px',
+});
+
+export const SignButtonTextStyle = style({
+  fontSize: '17px',
+});
