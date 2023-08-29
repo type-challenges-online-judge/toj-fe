fa23b5f3d7963d66802c36dc6ccc7e0ca744783a
minh0518 <rubics@naver.com>
2023-08-29 19:56:15 +0900

    [feat]: 선택한 문제 제공 기능 구현

---
+++ src/components/widget/probleminfo/ProblemInfo.css.ts
@@ -0,0 +1,33 @@
+import { style } from '@vanilla-extract/css';
+
+export const ProblemTitleStyle = style({
+  fontSize: '2.8rem',
+});
+
+const ProblemDiffFont = style({
+  fontSize: '1.5rem',
+  marginLeft: '30px',
+});
+export const ProblemDiffStyle = {
+  easy: style([ProblemDiffFont, { color: '#7BA918' }]),
+  medium: style([ProblemDiffFont, { color: '#D49228' }]),
+  hard: style([ProblemDiffFont, { color: '#D74640' }]),
+  extreme: style([ProblemDiffFont, { color: '#AC258C' }]),
+};
+
+export const ProblemCategoryStyle = style({
+  fontSize: '1.2rem',
+});
+
+export const ProblemCodeBlockStyle = style({
+  background: '#F5F5F5',
+  padding: '30px 0',
+  borderRadius: '10px',
+});
+
+export const ProblemCodeStyle = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '1.0rem',
+});
--- src/components/widget/probleminfo/ProblemInfo.tsx
+++ src/components/widget/probleminfo/ProblemInfo.tsx
@@ -1,25 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProblemList } from '../../../apis/get';
+import {
+  ProblemCategoryStyle,
+  ProblemCodeBlockStyle,
+  ProblemCodeStyle,
+  ProblemDiffStyle,
+  ProblemTitleStyle,
+} from './ProblemInfo.css';
 
+// 목데이터 전용으로 임시 생성.
+type problemInfoType = {
+  problemId: number;
+  problemDiff: 'easy' | 'medium' | 'hard' | 'extreme';
+  problemTitle: string;
+  problemDescription: string;
+  example: string[];
+  template: string[];
+  testCases: string[];
+};
 const ProblemInfo = () => {
   //const { problemId } = useParams();
-
   const problemId = 4;
-  const [problemInfo, setProblemInfo] = useState<any | null>(null);
+  const [problemInfo, setProblemInfo] = useState<problemInfoType | null>(null);
 
   useEffect(() => {
     const updateProblemList = async () => {
       try {
         const res = await getProblemList();
-        setProblemInfo(res);
+        const currentProblem = res.filter((i: any) => i.problemId === problemId)[0];
+        setProblemInfo(currentProblem);
       } catch (e) {}
     };
     updateProblemList();
   }, []);
 
-  console.log(problemInfo);
-  return <div>문제 페이지</div>;
+  return (
+    <div>
+      {problemInfo && (
+        <div>
+          <div>
+            <p className={ProblemTitleStyle}>
+              {problemInfo.problemTitle}
+              <span className={ProblemDiffStyle[problemInfo.problemDiff]}>
+                {problemInfo.problemDiff}
+              </span>
+            </p>
+          </div>
+
+          <p>{problemInfo.problemDescription}</p>
+
+          <div>
+            <p className={ProblemCategoryStyle}>예시</p>
+            <pre className={ProblemCodeBlockStyle}>
+              <code className={ProblemCodeStyle}>
+                {problemInfo.example.map((line) => `${line}\n`)}
+              </code>
+            </pre>
+          </div>
+
+          <div>
+            <p className={ProblemCategoryStyle}>제출 템플릿</p>
+
+            <pre className={ProblemCodeBlockStyle}>
+              <code className={ProblemCodeStyle}>
+                {problemInfo.template.map((line) => `${line}\n`)}
+              </code>
+            </pre>
+          </div>
+
+          <div>
+            <p className={ProblemCategoryStyle}>테스트 케이스</p>
+
+            <pre className={ProblemCodeBlockStyle}>
+              <code className={ProblemCodeStyle}>
+                {problemInfo.testCases.map((line) => `${line}\n`)}
+              </code>
+            </pre>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProblemInfo;
