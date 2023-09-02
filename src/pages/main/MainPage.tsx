6c126de75b5b1e6ab70e8eb6d21f561cd8e9e602
wisdomin121@naver.com <wisdomin121@naver.com>
2023-09-02 19:16:41 +0900

    feat: 메인페이지에 배너 추가

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,7 +1,12 @@
 import { MainPageStyle } from './MainPage.css';
+import { Banner } from '../../components';
 
 const MainPage = () => {
-  return <div className={MainPageStyle}></div>;
+  return (
+    <div className={MainPageStyle}>
+      <Banner />
+    </div>
+  );
 };
 
 export default MainPage;
