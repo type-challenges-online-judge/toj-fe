eb9ec8d861ca223fd233d8e05aacb50fde5bfd5a
wisdomin121@naver.com <wisdomin121@naver.com>
2023-09-04 18:30:00 +0900

    chore: Level 타입 적용 및 메인 페이지 문제 버튼에 사용할 타입들 추가

--- src/type/problem.ts
+++ src/type/problem.ts
@@ -1,7 +1,9 @@
+export type Level = 'warm-up' | 'easy' | 'medium' | 'hard' | 'extreme';
+
 // 목데이터 전용으로 임시 생성.
 export interface ProblemInfoType {
   problemId: number;
-  problemDiff: 'easy' | 'medium' | 'hard' | 'extreme';
+  problemDiff: Level;
   problemTitle: string;
   problemDescription: string;
   example: string[];
@@ -9,4 +11,17 @@ export interface ProblemInfoType {
   testCases: string[];
 }
 
-export type Level = 'warm-up' | 'easy' | 'medium' | 'hard' | 'extreme';
+export type MainProblem = Pick<ProblemInfoType, 'problemId' | 'problemTitle'>;
+
+export interface MainProblems {
+  'warm-up': MainProblem[];
+  easy: MainProblem[];
+  medium: MainProblem[];
+  hard: MainProblem[];
+  extreme: MainProblem[];
+}
+
+export interface MainLevel {
+  level: Level;
+  count: number;
+}
