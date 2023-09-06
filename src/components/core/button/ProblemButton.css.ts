d2a7c3116165c0c4b707ee61c95287db03d455b0
minh0518 <rubics@naver.com>
2023-09-06 11:55:50 +0900

    [feat]: 문제 페이지 TS온라인에서 풀이 버튼 추가

--- src/components/core/button/ProblemButton.css.ts
+++ src/components/core/button/ProblemButton.css.ts
@@ -1,1 +1,10 @@
 import { style } from '@vanilla-extract/css';
+
+export const MenuButton = style({
+  backgroundColor: 'transparent',
+  padding: '0 100px',
+  border: 'none',
+  borderRight: '1px solid black',
+  height: '100%',
+  cursor: 'pointer',
+});
---
+++ src/components/core/button/TsOnlineButton.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css';
+
+export const TsOnlineButtonStyle = style({
+  backgroundColor: '#3178C6',
+  fontSize: '0.9rem',
+  color: 'white',
+  border: 'none',
+  padding: '10px 20px',
+  borderRadius: '5px',
+  cursor: 'pointer',
+});
