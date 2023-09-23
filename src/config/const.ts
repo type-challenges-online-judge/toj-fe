a973f36c858ae08c8e18acc22740bafcfbe277b6
minh0518 <rubics@naver.com>
2023-09-23 18:00:29 +0900

    :bug: [bug]: 누락된 파일 추가

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,4 +1,5 @@
 export const API_URL = 'https://port-0-nestjs-147bpb2mlma5e3oj.sel5.cloudtype.app';
 
-export const GIT_HUB_LOGIN_URL =
-  'https://github.com/login/oauth/authorize?client_id=ec7011ef55fd1f295071';
+export const GIT_HUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${
+  import.meta.env.REACT_APP_GIT_HUB_CLIENT_ID
+}`;
