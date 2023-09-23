f69141563714b4a5afae8544dcf91bbef8d8b2ed
minh0518 <rubics@naver.com>
2023-09-23 18:14:32 +0900

    :sparkles: [feat]: 쿼리스트링 기반 문제 반환 MSW 메소드 추가
    제출 현황 페이지에서 쿼리스트링을 기반으로 문제들을 반환하는 MSW메소드를   추가 합니다

--- src/pages/status/Status.tsx
+++ src/pages/status/Status.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+// 내 제출
+// https://localhost:3000/status?result_id=1&problem_id=4&snsId=123&mine=true
+// problem_id , snsId , result_id=1(1이면 맞은 문제, -1이면 모두 ) , mine=true
+
+// 정답
+// https://localhost:3000/status?result_id=1&problem_id=4&snsId=123&mine=false
+// problem_id , snsId , result_id=1 , mine=false
 
 const Status = () => {
-  // 내 제출
-  // https://localhost:3000/status?result_id=-1&problem_id=4&user_id=minh0518&mine=true
-  // problem_id , user_id , result_id=-1(음수면 그냥 죄다 보여줌) , mine=true
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
 
-  // 정답
-  // https://localhost:3000/status?result_id=1&problem_id=4&user_id=minh0518&mine=false
-  // problem_id , user_id , result_id=1(1이면 맞은 문제들) , mine=false
+  const [resultId, problemId, snsId, mine] = [
+    Number(queryParams.get('result_id')),
+    Number(queryParams.get('problem_id')),
+    Number(queryParams.get('snsId')),
+    queryParams.get('mine') === 'true',
+  ];
+
+  useEffect(() => {
+    const getSubmitStatus = async () => {
+      const res = await axios.get('status', {
+        params: {
+          result_id: resultId,
+          problem_id: problemId,
+          snsId,
+          mine,
+        },
+      });
+    };
+    getSubmitStatus();
+  }, []);
 
   return <div>문제 기록 페이지 (내 제출 ,정답 보기)</div>;
 };
