b1dd6a16155010957e6868ea912c9846c46d058f
minh0518 <rubics@naver.com>
2023-10-05 16:52:37 +0900

    sourcemap 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
-  // build: {
-  //   sourcemap: true,
-  // },
+  build: {
+    sourcemap: true,
+  },
 });
