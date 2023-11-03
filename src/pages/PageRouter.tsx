64b5b3ac3f04c03ef9476f2d2cec1984575f4361
minh0518 <rubics@naver.com>
2023-11-03 14:37:35 +0900

    :recycle: [refactor]: 기존 라우팅 구조를 수정합니다
    CommonLayout을 하나로 합치고, 불필요한 부분을 생략합니다

--- src/pages/PageRouter.tsx
+++ src/pages/PageRouter.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter } from 'react-router-dom';
 // element
 import * as Switch from '@/pages';
 import { PAGE_URL } from '@/config/path';
-import { CommonLayout, CommonLayoutWithMenus } from '@/components/layout';
+import { CommonLayout } from '@/components/layout';
 import App from '@/App';
 import Authorization from '@/components/widget/common/Authorization';
 
@@ -13,26 +13,46 @@ interface RouterInfoType {
   element: ReactNode;
   withAuthorization: boolean;
   label: string;
+  isMenu: boolean;
 }
 
 const RouterInfo: RouterInfoType[] = [
   {
+    path: `${PAGE_URL.Login}`,
+    element: <Switch.LoginPage />,
+    withAuthorization: false,
+    label: '로그인 페이지',
+    isMenu: false,
+  },
+  {
+    path: `${PAGE_URL.Main}`,
+    element: <Switch.MainPage />,
+    withAuthorization: false,
+    label: '메인 페이지',
+    isMenu: false,
+  },
+  {
+    // 쿼리스트링 사용 페이지
     path: `${PAGE_URL.Problem}/:problemId`,
     element: <Switch.ProblemPage />,
     withAuthorization: false,
     label: '문제 정보 페이지',
+    isMenu: true,
   },
   {
+    // 쿼리스트링 사용 페이지
     path: `${PAGE_URL.Submit}/:problemId`,
     element: <Switch.Submit />,
     withAuthorization: true,
     label: '문제 제출 페이지',
+    isMenu: true,
   },
   {
     path: `${PAGE_URL.Status}`,
     element: <Switch.Status />,
     withAuthorization: true,
     label: '제출 현황 페이지',
+    isMenu: true,
   },
 ];
 
@@ -40,22 +60,12 @@ const router = createBrowserRouter([
   {
     path: `${PAGE_URL.Main}`,
     element: <App />,
-    children: [
-      {
-        path: '',
-        element: <CommonLayout />,
+    children: RouterInfo.map((routerInfo) => {
+      return {
+        // path 생략
+        element: <CommonLayout isMenu={routerInfo.isMenu} />,
         children: [
-          {
-            index: true,
-            element: <Switch.MainPage />,
-          },
-        ],
-      },
-      {
-        path: '/*',
-        element: <CommonLayoutWithMenus />,
-        children: RouterInfo.map((routerInfo) => {
-          return routerInfo.withAuthorization
+          routerInfo.withAuthorization
             ? {
                 path: routerInfo.path,
                 element: <Authorization>{routerInfo.element}</Authorization>,
@@ -63,14 +73,10 @@ const router = createBrowserRouter([
             : {
                 path: routerInfo.path,
                 element: routerInfo.element,
-              };
-        }),
-      },
-      {
-        path: `${PAGE_URL.Login}`,
-        element: <Switch.LoginPage />,
-      },
-    ],
+              },
+        ],
+      };
+    }),
   },
   {
     path: `${PAGE_URL.Login}/process`,
