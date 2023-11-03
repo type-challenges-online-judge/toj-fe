ea6f86e47c73fa48491ad95dd229e0d4ee6d0766
minh0518 <rubics@naver.com>
2023-11-03 14:36:48 +0900

    :recycle: [refactor]: CommonLayout컴포넌트 isMenu props추가
    문제 관련 정보들을 보여주는 메뉴바의 사용 여부를 isMenu  pops로 전달합니다

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,9 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
+
+// components
+import { ProblemMenus } from '@/components/widget';
+
+// util
+import { checkURL } from '@/util/problem';
+import { useGetProblemDetail } from '@/hooks/queries/problem';
+import { GetProblemDetailType } from '@/type/problem';
 import { CommonLayoutStyle } from './CommonLayout.css';
 
-const CommonLayout = () => {
+interface CommonLayoutProps {
+  isMenu: boolean;
+}
+
+const CommonLayout = ({ isMenu }: CommonLayoutProps) => {
+  const { problemId } = useParams();
+  const location = useLocation();
+  const [currentProblemId, setCurrentProblemId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const decideProblemId = () => {
+      if (!checkURL(location.pathname)) {
+        const problemIdFromLocalStorage = Number(JSON.parse(localStorage.getItem('problemId')!));
+        setCurrentProblemId(problemIdFromLocalStorage);
+      }
+
+      if (checkURL(location.pathname)) {
+        localStorage.setItem('problemId', JSON.stringify(problemId));
+        setCurrentProblemId(Number(problemId));
+      }
+    };
+    decideProblemId();
+  }, [location.pathname, problemId]);
+
+  const { data: { data: problemDetailData = null } = {} } =
+    useGetProblemDetail<GetProblemDetailType>(currentProblemId);
   return (
     <div className={CommonLayoutStyle}>
+      {problemDetailData !== null && isMenu && <ProblemMenus problemDetail={problemDetailData} />}
       <Outlet />
     </div>
   );
